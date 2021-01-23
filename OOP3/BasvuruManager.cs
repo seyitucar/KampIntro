0f93315b89a29e35ff1dc49baa2b2f94bc931da5
Seyit Uçar <sytucar@hotmail.com>
2021-01-24 00:04:13 +0300

    24.01.2021

---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        //method injection
+        public void BasvuruYap(IKrediManager krediManager, List<ILoggerService> loggerServices)
+        {
+            //Basvuran bilgileri değerlendirme
+            //
+            krediManager.Hesapla();
+            foreach (var loggerService in loggerServices)
+            {
+                loggerService.Log();
+            }
+        }
+
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+            }
+        }
+
+
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager esnafKrediManager = new EsnafKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+            ILoggerService smsLoggerService = new SmsLoggerService();
+
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager,tasitKrediManager};
+            List<ILoggerService> loggerServices = new List<ILoggerService>() {smsLoggerService,fileLoggerService };
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(esnafKrediManager, loggerServices);
+
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+
+
+
+        }
+    }
+}
